01b76c8a793d670103df336aae48160b8be8fc82
Aleksandr-Moskvichekov <81796316+Aleksandr-Moskvichekov@users.noreply.github.com>
2021-04-12 17:05:41 +0700

    Управление моторами
    Изменены названия , поставлены в порядке с верхнего левого по часовой стрелке

---
+++ Libraries/miniplatform/Motor2/Motor2.ino
@@ -0,0 +1,33 @@
+    
+#define FirstPin1 2
+#define FirstPin2 6
+#define SpeedPinFirstMotor 9
+
+#define SecondPin1 1
+#define SecondPin2 5
+#define SpeedPinSecondMotor 10
+
+#define ThirdPin1 44
+#define ThirdPin2 72
+#define SpeedPinTridMotor 11
+
+#define FourPin1 4
+#define FourPin2 8
+#define SpeedPinFourthMotor 12
+
+int Speed1 =0; 
+int Speed2 =0;
+int Speed3 =0;
+int Speed4 =0;
+#include "miniplatform.h"
+void setup() {
+  // put your setup code here, to run once:
+
+  setupMotorShield();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  goForward();
+
+}
---
+++ Libraries/miniplatform/Motor2/miniplatform.h
@@ -0,0 +1,150 @@
+void setupMotorShield()
+{
+  pinMode(SecondPin1,OUTPUT);
+  pinMode(SecondPin2,OUTPUT);
+  pinMode(SpeedPinSecondMotor,OUTPUT);
+  
+  pinMode(FirstPin1,OUTPUT);
+  pinMode(FirstPin2,OUTPUT);
+  pinMode(SpeedPinFirstMotor,OUTPUT);
+  
+  
+  pinMode(ThirdPin1,OUTPUT);
+  pinMode(ThirdPin2,OUTPUT);
+  pinMode(SpeedPinTridMotor,OUTPUT);
+  
+  pinMode(FourPin1,OUTPUT);
+  pinMode(FourPin2,OUTPUT);
+  pinMode(SpeedPinFourthMotor,OUTPUT);
+}
+void RotatesLeft()
+{
+  digitalWrite(SecondPin1,0);
+  digitalWrite(SecondPin2,1);
+  analogWrite(SpeedPinSecondMotor,Speed2);
+  
+  digitalWrite(FirstPin1,1);
+  digitalWrite(FirstPin2,0);
+  analogWrite(SpeedPinFirstMotor,Speed1);
+  
+  digitalWrite(ThirdPin1,0);
+  digitalWrite(ThirdPin2,1);
+  analogWrite(SpeedPinTridMotor,Speed3);
+  
+  digitalWrite(FourPin1,1);
+  digitalWrite(FourPin2,0);
+  analogWrite(SpeedPinFourthMotor,Speed4);
+
+}
+void RotatesRigt()
+{
+  digitalWrite(SecondPin1,1);
+  digitalWrite(SecondPin2,0);
+  analogWrite(SpeedPinSecondMotor,Speed2);
+
+  digitalWrite(FirstPin1,0);
+  digitalWrite(FirstPin2,1);
+  analogWrite(SpeedPinFirstMotor,Speed1);
+  
+  digitalWrite(ThirdPin1,1);
+  digitalWrite(ThirdPin2,0);
+  analogWrite(SpeedPinTridMotor,Speed3);
+  
+  digitalWrite(FourPin1,0);
+  digitalWrite(FourPin2,1);
+  analogWrite(SpeedPinFourthMotor,Speed4);
+
+}
+void Stop()
+{
+  digitalWrite(SecondPin1,0);
+  digitalWrite(SecondPin2,0);
+  analogWrite(SpeedPinSecondMotor,0);
+
+  digitalWrite(FirstPin1,0);
+  digitalWrite(FirstPin2,0);
+  analogWrite(SpeedPinFirstMotor,0);
+  
+  
+  digitalWrite(ThirdPin1,0);
+  digitalWrite(ThirdPin2,0);
+  analogWrite(SpeedPinTridMotor,0);
+  
+  digitalWrite(FourPin1,0);
+  digitalWrite(FourPin2,0);
+  analogWrite(SpeedPinFourthMotor,0);
+}
+void goForward()
+{
+  digitalWrite(SecondPin1,1);
+  digitalWrite(SecondPin2,0);
+  analogWrite(SpeedPinSecondMotor,Speed2);
+
+  digitalWrite(FirstPin1,1);
+  digitalWrite(FirstPin2,0);
+  analogWrite(SpeedPinFirstMotor,Speed1);
+  
+  digitalWrite(ThirdPin1,1);
+  digitalWrite(ThirdPin2,0);
+  analogWrite(SpeedPinTridMotor,Speed3);
+  
+  
+  digitalWrite(FourPin1,1);
+  digitalWrite(FourPin2,0);
+  analogWrite(SpeedPinFourthMotor,Speed4);
+}
+void turnLeft()
+{
+  digitalWrite(SecondPin1,1);
+  digitalWrite(SecondPin2,0);
+  analogWrite(SpeedPinSecondMotor,Speed2);
+
+  digitalWrite(FirstPin1,0);
+  digitalWrite(FirstPin2,1);
+  analogWrite(SpeedPinFirstMotor,Speed1);
+  
+  digitalWrite(ThirdPin1,0);
+  digitalWrite(ThirdPin2,1);
+  analogWrite(SpeedPinTridMotor,Speed3);
+  
+  digitalWrite(FourPin1,1);
+  digitalWrite(FourPin1,0);
+  analogWrite(SpeedPinFourthMotor,Speed4);
+}
+void turnRight()
+{
+  digitalWrite(SecondPin1,0);
+  digitalWrite(SecondPin2,1);
+  analogWrite(SpeedPinSecondMotor,Speed2);
+
+  digitalWrite(FirstPin1,1);
+  digitalWrite(FirstPin2,0);
+  analogWrite(SpeedPinFirstMotor,Speed1);
+
+  digitalWrite(ThirdPin1,1);
+  digitalWrite(ThirdPin2,0);
+  analogWrite(SpeedPinTridMotor,Speed3);
+  
+  digitalWrite(FourPin1,0);
+  digitalWrite(FourPin2,1);
+  analogWrite(SpeedPinFourthMotor,Speed4);
+}
+void back()
+{
+  digitalWrite(SecondPin1,0);
+  digitalWrite(SecondPin2,1);
+  analogWrite(SpeedPinSecondMotor,Speed2);
+
+  digitalWrite(FirstPin1,0);
+  digitalWrite(FirstPin2,1);
+  analogWrite(SpeedPinFirstMotor,Speed1);
+  
+  digitalWrite(ThirdPin1,0);
+  digitalWrite(ThirdPin2,1);
+  analogWrite(SpeedPinTridMotor,Speed3);
+  
+ 
+  digitalWrite(FourPin1,0);
+  digitalWrite(FourPin2,1);
+  analogWrite(SpeedPinFourthMotor,Speed4);
+}
